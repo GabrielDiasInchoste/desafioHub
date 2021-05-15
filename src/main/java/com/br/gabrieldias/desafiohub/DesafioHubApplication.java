909be9dee8a37a3d8980fa75fbf379d2c937e2e7
Gabriel Dias <gabriel.dias97@hotmail.com>
2021-05-15 14:04:16 -0300

    GD-01 Initial Commit

---
+++ src/main/java/com/br/gabrieldias/desafiohub/DesafioHubApplication.java
@@ -0,0 +1,13 @@
+package com.br.gabrieldias.desafiohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gabrieldias/desafiohub/DesafioHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gabrieldias.desafiohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
